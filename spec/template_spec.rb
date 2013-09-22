c9a4c134d8a5c59be3f9177b1fdf9dbbc9d8932a
nickl- <github@jigsoft.co.za>
2013-09-23 01:04:50 +0200

    Several rawjs example specs.

--- spec/template_spec.rb
+++ spec/template_spec.rb
@@ -43,6 +43,170 @@ describe RubyHamlJs::Template do
     end
   end
 
+  describe 'rawjs.if.simple' do
+    let(:choice) { true }
+    subject do
+      func = render <<eot, 'myTemplate.js.hamljs'
+- if (choice)
+  %p it is so
+eot
+      ExecJS.compile("var f = #{func};").eval "f({choice: #{choice}})"
+    end
+
+    it { should include '<p>it is so</p>' }
+
+    context "changing opinion" do
+      let(:choice) { false }
+      it { should_not include '<p>it is so</p>' }
+    end
+  end
+
+  describe 'rawjs.if.begin' do
+    let(:choice) { true }
+    subject do
+      func = render <<eot, 'myTemplate.js.hamljs'
+- if (choice) {
+  %p it is so
+eot
+      ExecJS.compile("var f = #{func};").eval "f({choice: #{choice}})"
+    end
+
+    it { should include '<p>it is so</p>' }
+
+    context "changing opinion" do
+      let(:choice) { false }
+      it { should_not include '<p>it is so</p>' }
+    end
+  end
+
+  describe 'rawjs.for.simple' do
+    let(:list) { [1,2,3] }
+    subject do
+      func = render <<eot, 'myTemplate.js.hamljs'
+- for (i=0; i < list.length; i++)
+  %p= list[i]
+eot
+      ExecJS.compile("var f = #{func};").eval "f({list: #{list}})"
+    end
+
+    it { should include '<p>1</p>' }
+    it { should include '<p>2</p>' }
+    it { should include '<p>3</p>' }
+
+  end
+
+  describe 'rawjs.for.begin' do
+    let(:list) { [1,2,3] }
+    subject do
+      func = render <<eot, 'myTemplate.js.hamljs'
+- for (i=0; i < list.length; i++) {
+  %p= list[i]
+eot
+      ExecJS.compile("var f = #{func};").eval "f({list: #{list}})"
+    end
+
+    it { should include '<p>1</p>' }
+    it { should include '<p>2</p>' }
+    it { should include '<p>3</p>' }
+
+  end
+
+  describe 'rawjs.for.in' do
+    let(:list) { [1,2,3] }
+    subject do
+      func = render <<eot, 'myTemplate.js.hamljs'
+- for (i in list)
+  %p= list[i]
+eot
+      ExecJS.compile("var f = #{func};").eval "f({list: #{list}})"
+    end
+
+    it { should include '<p>1</p>' }
+    it { should include '<p>2</p>' }
+    it { should include '<p>3</p>' }
+
+  end
+
+  describe 'rawjs.assign' do
+    let(:list) { [1,2,3] }
+    subject do
+      func = render <<eot, 'myTemplate.js.hamljs'
+- for (i=0; i < list.length; i++) {
+  - pval = list[i];
+  %p= pval
+eot
+      ExecJS.compile("var f = #{func};").eval "f({list: #{list}})"
+    end
+
+    it { should include '<p>1</p>' }
+    it { should include '<p>2</p>' }
+    it { should include '<p>3</p>' }
+
+  end
+
+  describe 'rawjs.for.complex' do
+    let(:list) { [1,2,3] }
+    subject do
+      func = render <<eot, 'myTemplate.js.hamljs'
+- for (i = 0; i < list.length; i++)
+  %i= i
+  - if (list[i] == 1)
+    %p one
+  - if (list[i] == 2)
+    %p two
+  - if (list[i] == 3)
+    %p three
+  %p= list[i]
+eot
+      ExecJS.compile("var f = #{func};").eval "f({list: #{list}})"
+    end
+
+    it { should include '<i>0</i>' }
+    it { should include '<i>1</i>' }
+    it { should include '<i>2</i>' }
+    it { should include '<p>1</p>' }
+    it { should include '<p>2</p>' }
+    it { should include '<p>3</p>' }
+    it { should include '<p>one</p>' }
+    it { should include '<p>two</p>' }
+    it { should include '<p>three</p>' }
+
+  end
+
+  describe 'rawjs.while' do
+    let(:list) { [1,2,3] }
+    subject do
+      func = render <<eot, 'myTemplate.js.hamljs'
+- i = 0;
+- while (i < list.length) {
+  %p= list[i]
+  - i++;
+eot
+      ExecJS.compile("var f = #{func};").eval "f({list: #{list}})"
+    end
+
+    it { should include '<p>1</p>' }
+    it { should include '<p>2</p>' }
+    it { should include '<p>3</p>' }
+
+  end
+
+  describe 'rawjs.async' do
+    let(:list) { [1,2,3] }
+    subject do
+      func = render <<eot, 'myTemplate.js.hamljs'
+- list.forEach(function (i) {
+  %p= i
+eot
+      ExecJS.compile("var f = #{func};").eval "f({list: #{list}})"
+    end
+
+    it { should include '<p>1</p>' }
+    it { should include '<p>2</p>' }
+    it { should include '<p>3</p>' }
+
+  end
+
   describe 'serving' do
     subject { assets }
     it { should serve 'sample.js' }
